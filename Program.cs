35098b3b94b3239bf2aa98a8c7d882ef0347c180
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 20:32:47 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿// Sumar números ingresados hasta que el total supere 100
+
+int numero;
+int total = 0;
+
+while (total <= 100)
+{
+    Console.WriteLine("Ingrese número positivos (presione 0 para salir)");
+    numero = int.Parse(Console.ReadLine());
+
+    total += numero;
+
+}
+Console.WriteLine("El total supero los 100");
+Console.WriteLine("El total final es = " + total);
